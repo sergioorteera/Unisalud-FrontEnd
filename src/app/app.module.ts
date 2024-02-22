0aff590e99ff39e55612daf6cee252fc7db9cec8
Sergio <sergioorteera@unicauca.edu.co>
2024-02-22 16:54:46 -0500

    Modificaciones en sidebar, header, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     SharedModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrincipalPageComponent } from './components/principal-page/principal-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     PrincipalPageComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     PrincipalPageComponent,
-    SidebarComponent,
   ]
 })
 export class SharedModule { }
