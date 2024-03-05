5765a9de5bee4cf9fe7e844e36028309c26be895
Sergio <sergioorteera@unicauca.edu.co>
2024-03-05 18:49:41 -0500

    avances

--- src/app/views/membership-application/components/membership-aplication/membership-aplication.component.ts
+++ src/app/views/membership-application/components/membership-aplication/membership-aplication.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class MembershipAplicationComponent {
 
+  activeTabIndex = 0;
+
+  goBack() {
+    if (this.activeTabIndex > 0) {
+      this.activeTabIndex--;
+    }
+  }
+
+  goForward() {
+    if (this.activeTabIndex < 3) {
+      this.activeTabIndex++;
+    }
+  }
+
+  isFirstTab() {
+    return this.activeTabIndex === 0;
+  }
+
+  isLastTab() {
+    return this.activeTabIndex === 3;
+  }
+
+  getTabClass(index: number): string {
+    return this.activeTabIndex === index ? 'active-tab' : '';
+  }
+
 }
